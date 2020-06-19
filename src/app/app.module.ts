b4a8520ab8cb15ae460a36263b11bc44b983a2b4
Jared Brink <jaredbrink86@gmail.com>
2020-06-19 13:14:52 -0600

    Added date picker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
 
 import { AppComponent } from "./app.component";
 import { TransactionFormComponent } from "./transaction-form/transaction-form.component";
 import { HeaderComponent } from "./header/header.component";
 import { TransactionsComponent } from "./transactions/transactions.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,13 @@ import { TransactionsComponent } from "./transactions/transactions.component";
     HeaderComponent,
     TransactionsComponent,
   ],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDatepickerModule.forRoot(),
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
